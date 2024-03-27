649ceb85984b7742e9ff10ee084dd6becc01b4e6
SunWoo <sunwoo3856@naver.com>
2024-03-27 15:29:40 +0900

    Initial commit

---
+++ src/main/java/com/coders/commagateway/CommaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.coders.commagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommaGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coders/commagateway/CommaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coders.commagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
