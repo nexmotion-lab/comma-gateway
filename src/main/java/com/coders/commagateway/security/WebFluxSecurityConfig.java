b5a1af8f4a17bf5108e9ebbabadf6901828bc8e3
SunWoo <sunwoo3856@naver.com>
2024-08-20 16:36:29 +0900

    home route

--- src/main/java/com/coders/commagateway/security/WebFluxSecurityConfig.java
+++ src/main/java/com/coders/commagateway/security/WebFluxSecurityConfig.java
@@ -71,6 +71,7 @@ public class WebFluxSecurityConfig {
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
                 HttpMethod.DELETE.name(),
                 HttpMethod.OPTIONS.name()
         ));
