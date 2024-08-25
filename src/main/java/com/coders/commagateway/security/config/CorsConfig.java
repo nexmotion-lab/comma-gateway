65671940c1a23ad2892894c24f6c80325d132e4e
SunWoo <sunwoo3856@naver.com>
2024-08-25 15:17:29 +0900

    home route

--- src/main/java/com/coders/commagateway/security/config/CorsConfig.java
+++ src/main/java/com/coders/commagateway/security/config/CorsConfig.java
@@ -1,38 +1,33 @@
 package com.coders.commagateway.security.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-@Configuration
+@Component
 @RefreshScope
+@ConfigurationProperties(prefix = "cors")
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins}")
     private List<String> allowedOrigins;
-
-    @Value("${cors.allowed-methods}")
     private List<String> allowedMethods;
-
-    @Value("${cors.allowed-headers}")
     private List<String> allowedHeaders;
-
-    @Value("${cors.allow-credentials}")
     private boolean allowCredentials;
-
-    @Value("${cors.exposed-headers}")
     private List<String> exposedHeaders;
-
-    @Value("${cors.max-age}")
     private long maxAge;
 
     @Bean
+    @RefreshScope
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(allowedOrigins);
@@ -48,3 +43,4 @@ public class CorsConfig {
         return source;
     }
 }
+
